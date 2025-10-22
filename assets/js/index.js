15d280af18a2f62bc3b98b0398c370167e3c8c30
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-10-22 22:08:52 +0400

    first

---
+++ assets/js/index.js
@@ -0,0 +1,77 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  const headerTop = document.querySelector("header");
+  if (window.scrollY > 0) {
+    headerTop.classList.add("moved");
+  } else {
+    headerTop.classList.remove("moved");
+  }
+});
+
+
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const savedTheme = localStorage.getItem("theme");
+
+const currentTheme = savedTheme || (prefersDark ? "dark" : "light");
+document.documentElement.setAttribute("data-theme", currentTheme);
+updateImages(currentTheme);
+
+function updateImages(theme) {
+  const images = document.querySelectorAll("img[data-light][data-dark]");
+  images.forEach(img => {
+    img.src = theme === "dark" ? img.dataset.dark : img.dataset.light;
+  });
+}
+
+const lightBtn = document.querySelector(".icon-light-switch");
+const darkBtn = document.querySelector(".icon-dark-switch");
+
+darkBtn.addEventListener("click", () => {
+  document.documentElement.setAttribute("data-theme", "dark");
+  updateImages("dark");
+});
+
+lightBtn.addEventListener("click", () => {
+  document.documentElement.setAttribute("data-theme", "light");
+  updateImages("light");
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    let elem = document.querySelector('.reviews-items');
+    let msnry = new Masonry( elem, {
+        itemSelector: '.reviews-item',
+        columnWidth: 10,
+        gutter: 20,
+        percentPosition: true,
+
+    });
+
+
+
+});
+
+
+
+
+function toggleActiveState(item) {
+    const allItems = document.querySelectorAll('.often-asks-item');
+
+    allItems.forEach(otherItem => {
+        if (otherItem !== item) {
+            otherItem.classList.remove('active');
+        }
+    });
+
+    item.classList.toggle('active');
+}
+
+document.querySelectorAll('.often-asks-item').forEach(item => {
+    item.addEventListener('click', () => toggleActiveState(item));
+    const icon = item.querySelector('.asks-list-icon');
+    icon.addEventListener('click', (event) => {
+        event.stopPropagation();
+        toggleActiveState(item);
+    });
+});
