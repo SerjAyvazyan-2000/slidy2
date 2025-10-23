4b5cdaed476c877a4281d9798f675fa533735cc2
Serzh Ayvazyan <ayvazyanserj3@gmail.com>
2025-10-23 19:15:27 +0400

    first

--- assets/js/index.js
+++ assets/js/index.js
@@ -75,3 +75,27 @@ document.querySelectorAll('.often-asks-item').forEach(item => {
         toggleActiveState(item);
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const animatedItems = document.querySelectorAll(
+    ".fade-left, .fade-right, .fade-top, .fade-bottom"
+  );
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+        
+        }
+      });
+    },
+    {
+      threshold: 0.15, // элемент виден на 15%
+    }
+  );
+
+  animatedItems.forEach((item) => observer.observe(item));
+});
